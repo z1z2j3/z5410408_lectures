3c001dfe8479dc675fadb24668a6767490b5a5f7
Zijun Zhao <z15637780805@163.com>
2023-04-07 02:50:44 +1000

    Initial commit

---
+++ toolkit_config.py
@@ -0,0 +1,11 @@
+""" toolkit_config.py
+
+Project configuration file
+
+***do not submit this file***
+"""
+
+import os
+
+PRJDIR = r"/Users/zhaozijun/PycharmProjects/PROJECT"
+DATADIR = os.path.join(PRJDIR, 'data')
